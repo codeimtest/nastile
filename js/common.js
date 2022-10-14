943252c4cfd766cf2d140aca2831fc9e3e503206
Andrey <codeim@yandex.ru>
2022-10-14 11:14:07 +0300

    q

--- js/common.js
+++ js/common.js
@@ -1,48 +1,9 @@
-$('.reset').on('click',function(){
-  $('.matrix-title').addClass('pre_appear')
-  setTimeout(function(){
-    $('.matrix-title').removeClass('pre_appear')
-  }, 500)
-  
-})
-
-$.fn.isInViewport = function() {
-	var elementTop = $(this).offset().top;
-	var elementBottom = elementTop + $(this).outerHeight();
-
-	var viewportTop = $(window).scrollTop();
-	var viewportBottom = viewportTop + $(window).height();
-
-	return elementBottom > viewportTop && elementTop < viewportBottom;
-};
-var isEvent = false;
-
-$(window).scroll( function() {
-		if ($('.matrix-title').isInViewport()  ) {
-
-				$(this).addClass('pre_appear')
-
-		} 
-
+AOS.init({
+  duration: 600,
+  easing: 'fade',
+	once: true
 });
 
-/*
-
-$(window).on('resize scroll', function() {
-	
-	if ($(this).isInViewport() && !isEvent ) {
-		$('.matrix-title').each(function(){
-			isEvent = true;
-			$(this).addClass('pre_appear')
-			setTimeout(function(){
-				$(this).removeClass('pre_appear')
-			}, 500)
-		})
-		
-	} 
-});
-
-*/
 //toggle header
 $(window).scroll(function() {    
 	var scroll = $(window).scrollTop();
